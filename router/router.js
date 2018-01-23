5c5290e37dc6ca5c223e7cfc70e9986f683cbc17
792884274 <792884274@qq.com>
2018-01-23 20:58:05 +0800

    update

--- router/router.js
+++ router/router.js
@@ -98,6 +98,7 @@ exports.login=function (req,res,next) {
             var username=fields.username;
             var password=fields.password;
             var password_md5=md5(md5(password)+'792884274');
+            console.log(username);
             mysql(user_sql.select('username'),[username],function (err,result) {
                 if (err) {
                     res.send('-3');//服务器错误
